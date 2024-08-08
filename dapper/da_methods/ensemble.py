bdd47aeee9c7a8b91625a11e4c4ffe87924180b3
dgrund <dgrund@student.ethz.ch>
2024-08-08 14:45:48 +0200

    REVERT MASTER to initially forked version (moving changes to branch dapper-incl-pycles)

--- dapper/da_methods/ensemble.py
+++ dapper/da_methods/ensemble.py
@@ -35,8 +35,6 @@ class EnKF:
     N: int
 
     def assimilate(self, HMM, xx, yy):
-        self.name = f'EnKF_N{self.N}'
-
         # Init
         E = HMM.X0.sample(self.N)
         self.stats.assess(0, E=E)
@@ -372,8 +370,6 @@ class EnKS:
         return E.reshape((N, K, Nx)).transpose([1, 0, 2])
 
     def assimilate(self, HMM, xx, yy):
-        self.name = f'EnKS_N{self.N}'
-
         # Inefficient version, storing full time series ensemble.
         # See iEnKS for a "rolling" version.
         E    = zeros((HMM.tseq.K+1, self.N, HMM.Dyn.M))
@@ -626,8 +622,6 @@ class LETKF:
     mp: bool   = False
 
     def assimilate(self, HMM, xx, yy):
-        self.name = f'LETKF_N{self.N}'
-
         E = HMM.X0.sample(self.N)
         self.stats.assess(0, E=E)
         self.stats.new_series("ad_inf", 1, HMM.tseq.Ko+1)
@@ -859,7 +853,6 @@ class EnKF_N:
     g: int     = 0
 
     def assimilate(self, HMM, xx, yy):
-        self.name = f'EnKF-N_N{self.N}'
         N, N1 = self.N, self.N-1
 
         # Init
